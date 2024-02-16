8c878a414c9f7a8bdef4ffb7e0336a230ff3ed82
JoaquinMoyano1 <joaquinemoyanoi@gmail.com>
2024-02-15 21:12:08 -0300

    ultimo guardado solucion descuento stock

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,21 +1,14 @@
-import { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import ItemCount from '../ItemCount/ItemCount';
 import { Link } from 'react-router-dom';
 import { Button, Card } from 'react-bootstrap';
 import './ItemDetail.css';
-
-
 import { CarritoContext } from '../../context/CarritoContext';
 
-
-import { useContext } from 'react';
-
 const ItemDetail = ({ id, nombre, stock, precio, img, descripcion }) => {
-  const [agregarCantidad, setAgregarCantidad] = useState(0);
   const { agregarAlCarrito } = useContext(CarritoContext);
 
   const manejadorCantidad = (cantidad) => {
-    setAgregarCantidad(cantidad);
     const item = { id, nombre, precio, img };
     agregarAlCarrito(item, cantidad);
   }
@@ -31,13 +24,7 @@ const ItemDetail = ({ id, nombre, stock, precio, img, descripcion }) => {
           <span className="descripcion"> {descripcion}</span>
         </Card.Text>
 
-        {agregarCantidad > 0 ? (
-          <Link to="/cart">
-            <Button variant="primary">Terminar compra</Button>
-          </Link>
-        ) : (
-          <ItemCount incial={1} stock={stock} funcionAgregar={manejadorCantidad} />
-        )}
+        <ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad} />
 
         <Link to="/">
           <Button variant="secondary">Ver más productos</Button>
